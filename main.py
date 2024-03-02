8f509a5a6665c80fff40942c9024947ca15ce847
stephen.easley-walsh <stephen.easley-walsh@studygroup.com>
2024-03-02 18:37:49 +0000

    Update

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
 class line:
+    # Constructor
     def __init__(self, X1, Y1, X2, Y2):
         self.x1 = X1
         self.y1 = Y1
         self.x2 = X2
         self.y2 = Y2
 
+    # Setter
     def setX1(self, X1):
         self.x1 = X1
 
+    # Setter
     def setY1(self, Y1):
         self.y1 = Y1
 
+    # Setter
     def setX2(self, X2):
         self.x2 = X2
 
+    # Setter
     def setY2(self, Y2):
         self.y2 = Y2
 
+    # Getter
     def getX1(self):
         return self.x1
 
+    # Getter
     def getY1(self):
         return self.y1
 
+    # Getter
     def getX2(self):
         return self.x2
 
+    # Getter
     def getY2(self):
         return self.y1
 
+    # xCross method for finding the X-axis intersect
     def xCross(self):
         if not self.x2 == self.x1:
             m = (self.y2 - self.y1) / (self.x2 - self.x1)
@@ -41,6 +51,7 @@ class line:
             #  if x1=x2 then it is a vertical line
             return self.x1
 
+    # yCross method for finding the Y-axis intersect
     def yCross(self):
         if not self.x2 == self.x1:
             m = (self.y2 - self.y1) / (self.x2 - self.x1)
@@ -50,17 +61,21 @@ class line:
             raise ValueError("Error, x1 and x2 can't be the same value i.e. no intersection with Y-axis")
 
 
+# Child class point of parent class line
 class point(line):
     def __int__(self, X1, Y1):
         super().__init__(X1, Y1, X1, Y1)
 
+    # xCross doesn't apply, just pass instead of return
     def xCross(self):
         pass
 
+    # yCross doesn't apply, just pass instead of return
     def yCross(self):
         pass
 
 
+# This function asks for LINES, stores them in a list and returns the list
 def askLine():
     lines = []
     while True:
@@ -75,6 +90,7 @@ def askLine():
             return lines
 
 
+# This checks if any line in the list of lines crosses at either x or y (on the axis)
 def checkLines(lines, x, y):
     for line in lines:
         if line.xCross() == x or line.yCross() == y:
@@ -82,5 +98,6 @@ def checkLines(lines, x, y):
     return False
 
 
+# Take two points and make a line from them
 def makeLine(point1, point2):
     return line(point1.getX1(), point1.getY1(), point2.getX2(), point2.getY2())
