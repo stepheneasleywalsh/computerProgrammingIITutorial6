7c2a13bf1a05d21b5fbfe2f4e515fdf11d5d554c
stephen.easley-walsh <stephen.easley-walsh@studygroup.com>
2024-03-02 18:41:27 +0000

    Update

--- main.py
+++ main.py
@@ -1,45 +1,35 @@
 class line:
-    # Constructor
-    def __init__(self, X1, Y1, X2, Y2):
+    def __init__(self, X1, Y1, X2, Y2):     # Constructor
         self.x1 = X1
         self.y1 = Y1
         self.x2 = X2
         self.y2 = Y2
 
-    # Setter
-    def setX1(self, X1):
+    def setX1(self, X1):     # Setter
         self.x1 = X1
 
-    # Setter
-    def setY1(self, Y1):
+    def setY1(self, Y1):     # Setter
         self.y1 = Y1
 
-    # Setter
-    def setX2(self, X2):
+    def setX2(self, X2):     # Setter
         self.x2 = X2
 
-    # Setter
-    def setY2(self, Y2):
+    def setY2(self, Y2):     # Setter
         self.y2 = Y2
 
-    # Getter
-    def getX1(self):
+    def getX1(self):     # Getter
         return self.x1
 
-    # Getter
-    def getY1(self):
+    def getY1(self):  # Getter
         return self.y1
 
-    # Getter
-    def getX2(self):
+    def getX2(self): # Getter
         return self.x2
 
-    # Getter
-    def getY2(self):
+    def getY2(self): # Getter
         return self.y1
 
-    # xCross method for finding the X-axis intersect
-    def xCross(self):
+    def xCross(self):     # xCross method for finding the X-axis intersect
         if not self.x2 == self.x1:
             m = (self.y2 - self.y1) / (self.x2 - self.x1)
             if not m == 0:
@@ -51,32 +41,27 @@ class line:
             #  if x1=x2 then it is a vertical line
             return self.x1
 
-    # yCross method for finding the Y-axis intersect
-    def yCross(self):
+    def yCross(self):     # yCross method for finding the Y-axis intersect
         if not self.x2 == self.x1:
             m = (self.y2 - self.y1) / (self.x2 - self.x1)
-            y = self.y1 - m * self.x1
+            return self.y1 - m * self.x1
         else:
             # vertical line so no intersection
             raise ValueError("Error, x1 and x2 can't be the same value i.e. no intersection with Y-axis")
 
 
-# Child class point of parent class line
-class point(line):
+class point(line): # Child class point of parent class line
     def __int__(self, X1, Y1):
         super().__init__(X1, Y1, X1, Y1)
 
-    # xCross doesn't apply, just pass instead of return
-    def xCross(self):
+    def xCross(self): # xCross doesn't apply, just pass instead of return
         pass
 
-    # yCross doesn't apply, just pass instead of return
-    def yCross(self):
+    def yCross(self): # yCross doesn't apply, just pass instead of return
         pass
 
 
-# This function asks for LINES, stores them in a list and returns the list
-def askLine():
+def askLine(): # This function asks for LINES, stores them in a list and returns the list
     lines = []
     while True:
         try:
@@ -90,14 +75,12 @@ def askLine():
             return lines
 
 
-# This checks if any line in the list of lines crosses at either x or y (on the axis)
-def checkLines(lines, x, y):
+def checkLines(lines, x, y): # This checks if any line in the list of lines crosses at either x or y (on the axis)
     for line in lines:
         if line.xCross() == x or line.yCross() == y:
             return True
     return False
 
 
-# Take two points and make a line from them
-def makeLine(point1, point2):
+def makeLine(point1, point2): # Take two points and make a line from them
     return line(point1.getX1(), point1.getY1(), point2.getX2(), point2.getY2())
